a68be0c24c4e170b225ac647cf948a1629391f72
Ghalileo <ose.okogbo@gmail.com>
2022-01-30 12:52:13 -0600

    migrated from css to scss in order install additional packages compatible with scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tic-tac-toe'`, () => {
+  it(`should have as title 'Tic-Tac-Toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tic-tac-toe');
+    expect(app.title).toEqual('Tic-Tac-Toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Tic-Tac-Toe app is running!');
   });
 });
--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+  styleUrls: ['./square.component.scss']
 })
 export class SquareComponent implements OnInit {
   @Input() value!: 'X' | 'O';
