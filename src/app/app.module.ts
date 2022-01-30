aa39b63addd5ed0123ec32397b9e7c1a4a58c121
Ghalileo <ose.okogbo@gmail.com>
2022-01-30 11:59:43 -0600

    began working on functions for the board.  need to fix type string null error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './components/square/square.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
